23fea8c333a30b586a7853defa66dd211d29eee9
Shanley Grace Rosero <shanleyrosero@gmail.com>
2022-06-29 12:07:08 +0800

    Let this be a commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to React, Shan!
+          Welcome to React, Shanley!
         </p>
         <a
           className="App-link"
