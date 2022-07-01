04061d4fcb43a855c3e1854af4fa227aa6029a35
Shanley Grace Rosero <shanleyrosero@gmail.com>
2022-07-01 10:33:07 +0800

    I don't want this change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import React, { Component } from 'react';
 
-<<<<<<< HEAD
 function App() {
   return (
     <div className="App">
@@ -21,45 +20,6 @@ function App() {
       </header>
     </div>
   );
-=======
-class App extends Component {
-  constructor(props) {
-    super(props)
-    //state
-    this.state = {
-      users:[],
-      loading: false
-    }
-  }
-  
-  getUser() {
-    this.setState({
-      loading:true
-    })
-    axios('https://api.randomuser.me/?nat=US&results=5').then(response => 
-      this.setState({
-        users: response.data.results,
-        loading: false
-      })
-    );
-  }
-
-  componentDidMount() {
-    this.getUser()
-  }
-
-  render () {
-    return <div className="App">
-      {!this.state.loading? this.state.users.map(user => 
-        <div key={user.login.uuid}>
-          <p><strong>Name:</strong> {user.name.first} {user.name.last}</p>
-          <p><strong>Email:</strong> {user.email}</p>
-          <hr />
-        </div>
-      ): <div>Loading...</div>}
-    </div>;
-  } 
->>>>>>> remote
 }
 
 export default App;
