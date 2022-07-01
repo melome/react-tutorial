ea659ffe0538275ee38491a900e96d8b16fd7051
Shanley Grace Rosero <shanleyrosero@gmail.com>
2022-07-01 09:09:32 +0800

    1.Fetched data from randomuser API using axios 2.Deleted some unecessary files 3.Rendered randomuser data 4.Added loading functionality using setState()

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to React!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    //state
+    this.state = {
+      users:[],
+      loading: false
+    }
+  }
+  
+  getUser() {
+    this.setState({
+      loading:true
+    })
+    axios('https://api.randomuser.me/?nat=US&results=5').then(response => 
+      this.setState({
+        users: response.data.results,
+        loading: false
+      })
+    );
+  }
+
+  componentDidMount() {
+    this.getUser()
+  }
+
+  render () {
+    return <div className="App">
+      {!this.state.loading? this.state.users.map(user => 
+        <div key={user.login.uuid}>
+          <p><strong>Name:</strong> {user.name.first} {user.name.last}</p>
+          <p><strong>Email:</strong> {user.email}</p>
+          <hr />
+        </div>
+      ): <div>Loading...</div>}
+    </div>;
+  } 
 }
 
 export default App;
